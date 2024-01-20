882a9e91f2e8ac9ad35d5449f665a5df9d703a20
Ashik19aug <ashik19aug@gmail.com>
2024-01-20 22:56:09 +0600

    TypeScript : Define Type Aliases

---
+++ src/Type_Aliases/Type_Aliases.ts
@@ -0,0 +1,3 @@
+// In TypeScript, type aliases allow you to create a custom name for any type.
+// This can be especially useful when you have complex types or when you want to reuse types in multiple places.
+// You can create a type alias using the type keyword.
